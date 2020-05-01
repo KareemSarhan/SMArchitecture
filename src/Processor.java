9ffee1ff3ac437b83e957f1b80be4cfa0ef66237
NinjaKat <kareemsarhan@gmail.com>
2020-05-02 01:03:16 +0200

    init repo
    added java project

---
+++ src/Processor.java
@@ -0,0 +1,17 @@
+/**
+ * Processor
+ *  This class calls all the stages in our project
+ */
+public class Processor {
+/**
+ * Write comments you want to leave here !!
+*/
+/**
+ * Describe any added variable's use and write an one example or more to what it will contain
+*/
+
+    public static void main(String[] args) 
+    {
+        
+    }  
+}
