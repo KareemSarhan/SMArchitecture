044a4efd467b8e78f832f7bdb883461ba404632d
NinjaKat <kareemsarhan@gmail.com>
2020-05-03 01:26:02 +0200

    init

--- src/Processor.java
+++ src/Processor.java
@@ -1,17 +1,16 @@
 /**
- * Processor
- *  This class calls all the stages in our project
+ * Processor This class calls all the stages in our project
  */
 public class Processor {
-/**
- * Write comments you want to leave here !!
-*/
-/**
- * Describe any added variable's use and write an one example or more to what it will contain
-*/
+	/**
+	 * Write comments you want to leave here !!
+	 */
+	/**
+	 * Describe any added variable's use and write an one example or more to what it
+	 * will contain
+	 */
 
-    public static void main(String[] args) 
-    {
-        
-    }  
+	public static void main(String[] args) {
+		System.out.println("SMArchitecture");
+	}
 }
