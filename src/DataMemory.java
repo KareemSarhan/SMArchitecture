3d53d6a8549b50f1deb5f0b250fef6c30e961f8a
NinjaKat <kareemsarhan@gmail.com>
2020-05-04 09:31:57 +0200

    update

--- src/DataMemory.java
+++ src/DataMemory.java
@@ -1,57 +1,51 @@
 
 public class DataMemory {
-	String MemRead;
-	String MemWrite;
-	String ad;
-	String data;
+	
+	Processor processor;
 	Block[] cache;
 	String[] memory;
-	public DataMemory () {
-	cache=new Block[128];
-	memory= new String[1024];
+	
+	public DataMemory(Processor processor) {
+		this.processor = processor;
+		cache=new Block[128];
+		memory= new String[1024];
     }
 	
-	public String Readdata(int address) {
+	public void readData(int address) {
 		if(cache[address%128]!=null && cache[address%128].validBit==1) {
 			if(cache[address%128].tag==address/128) {
-				return cache[address%128].data;
+				processor.setMemoryReadData(cache[address%128].data);
+				return;
 			}
 			cache[address%128].data=memory[address];
 			cache[address%128].tag=address/128;
 			cache[address%128].validBit=1;
-			return memory[address];
-			}
+			processor.setMemoryReadData(memory[address]);
+			return;
+		}
 		cache[address%128]=new Block();
 		cache[address%128].data=memory[address];
 		cache[address%128].tag=address/128;
 		cache[address%128].validBit=1;
-		return memory[address];
+		processor.setMemoryReadData(memory[address]);
 	}
 	
-	public void writedata(int address, String data1) {
+	
+	public void writeData(int address, String data) {
 		if(cache[address%128]!=null && cache[address%128].validBit==1) {
-			cache[address%128].data=data1;
 			if(cache[address%128].tag!=address/128) {
+				cache[address%128].data=memory[address];
 				cache[address%128].tag=address/128;
 			}
+			cache[address%128].data=data;
 		}
 		else {
 			cache[address%128]=new Block();
-			cache[address%128].data=data1;
+			cache[address%128].data=data;
 			cache[address%128].tag=address/128;
 			cache[address%128].validBit=1;
 		}
-		memory[address]=data1;
-}
-	
-	public void memoryaccess() {
-		int address = Integer.parseInt(ad);
-		if(MemRead.equals("1")) {
-			String dataread = Readdata(address);
-			dataread=dataread+"";
-		}
-		if(MemWrite.equals("1")) {
-			writedata(address, data);
-		}
+		memory[address]=data;
 	}
+	
 }
