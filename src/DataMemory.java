ac3449af77957252ddffb0f990c1c2d348adf4e6
NinjaKat <kareemsarhan@gmail.com>
2020-05-05 10:54:38 +0200

    refactor

--- src/DataMemory.java
+++ src/DataMemory.java
@@ -5,28 +5,32 @@ public class DataMemory {
 	Block[] cache;
 	String[] memory;
 	
+	
 	public DataMemory(Processor processor) {
 		this.processor = processor;
-		cache=new Block[128];
-		memory= new String[1024];
+		cache = new Block[128];
+		memory = new String[1024];
+		for (int i=0 ; i<memory.length ; i++) {
+			memory[i] = Processor.convertDecToBinUnsigned(i);
+		}
     }
 	
 	public void readData(int address) {
 		if(cache[address%128]!=null && cache[address%128].validBit==1) {
-			if(cache[address%128].tag==address/128) {
+			if(cache[address%128].tag == address/128) {
 				processor.setMemoryReadData(cache[address%128].data);
 				return;
 			}
-			cache[address%128].data=memory[address];
-			cache[address%128].tag=address/128;
-			cache[address%128].validBit=1;
+			cache[address%128].data = memory[address];
+			cache[address%128].tag = address/128;
+			cache[address%128].validBit = 1;
 			processor.setMemoryReadData(memory[address]);
 			return;
 		}
-		cache[address%128]=new Block();
-		cache[address%128].data=memory[address];
-		cache[address%128].tag=address/128;
-		cache[address%128].validBit=1;
+		cache[address%128] = new Block();
+		cache[address%128].data = memory[address];
+		cache[address%128].tag = address/128;
+		cache[address%128].validBit = 1;
 		processor.setMemoryReadData(memory[address]);
 	}
 	
